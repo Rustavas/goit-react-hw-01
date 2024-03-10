22d80c8fcdc39d42821f0e368f6dcc8e31859685
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-03-10 22:42:47 +0200

    fixed Friendlist bag

--- src/components/friendsList/FriendListItem.jsx
+++ src/components/friendsList/FriendListItem.jsx
@@ -6,7 +6,11 @@ const FriendListItem = ({ avatar, name, isOnline = false }) => {
     <div className={css.friendListItem}>
       <img src={avatar} alt="Avatar" width="90" />
       <p>{name}</p>
-      {isOnline ? <p className={css.isonline}>Online</p>: <p className={css.offline}>Offline</p>}
+      {isOnline ? (
+        <p className={css.isonline}>Online</p>
+      ) : (
+        <p className={css.offline}>Offline</p>
+      )}
     </div>
   );
 };
